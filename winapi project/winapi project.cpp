a27849b97123fe3bb9c1c65b6993946c6ad698ee
david4more <domanov.david@gmail.com>
2025-06-09 17:59:17 +0200

    added optimized resizing logic

--- winapi project/winapi project.cpp
+++ winapi project/winapi project.cpp
@@ -20,31 +20,26 @@ LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
 // My variables
-void draw(HDC& hdc, PAINTSTRUCT& ps);
-struct msg
-{
-	msg(int x, int y) : x(x), y(y) {};
-	int x, y;
-	static const wchar_t* str;
-};
-const wchar_t* msg::str = L"hehe";
-
-std::vector<msg> messages;
+void draw(HDC& hdc, HWND hWnd);
+void updateCoords(HWND hWnd, int& centerX, int& centerY, int& buttonHeight, int& buttonWidth);
+void updateFontSize(int& fontSize);
+int centerX, centerY;
 HWND hButton;
-int buttonHeight;
-int buttonWidth;
-HBRUSH	hbrBackground = NULL;
+int buttonHeight, buttonWidth, fontSize;
+HBRUSH hbrBackground = NULL;
+HFONT buttonFont = NULL;
+RECT rect;
 
-int countdown = 300;
+int countdown = 25 * 60;
 bool timerState = false;
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-					 _In_opt_ HINSTANCE hPrevInstance,
-					 _In_ LPWSTR    lpCmdLine,
-					 _In_ int       nCmdShow)
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR    lpCmdLine,
+	_In_ int       nCmdShow)
 {
 	UNREFERENCED_PARAMETER(hPrevInstance);
-	UNREFERENCED_PARAMETER(lpCmdLine);  
+	UNREFERENCED_PARAMETER(lpCmdLine);
 
 	// TODO: Place code here.
 
@@ -54,7 +49,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	MyRegisterClass(hInstance);
 
 	// Perform application initialization:
-	if (!InitInstance (hInstance, nCmdShow))
+	if (!InitInstance(hInstance, nCmdShow))
 	{
 		return FALSE;
 	}
@@ -73,7 +68,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 		}
 	}
 
-	return (int) msg.wParam;
+	return (int)msg.wParam;
 }
 
 
@@ -91,17 +86,17 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 
 	wcex.cbSize = sizeof(WNDCLASSEX);
 
-	wcex.style          = CS_HREDRAW | CS_VREDRAW;
-	wcex.lpfnWndProc    = WndProc;
-	wcex.cbClsExtra     = 0;
-	wcex.cbWndExtra     = 0;
-	wcex.hInstance      = hInstance;
-	wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WINAPIPROJECT));
-	wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-	wcex.hbrBackground	= hbrBackground;
-	wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_WINAPIPROJECT);
-	wcex.lpszClassName  = szWindowClass;
-	wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WINAPIPROJECT));
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground = hbrBackground;
+	wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_WINAPIPROJECT);
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
 	return RegisterClassExW(&wcex);
 }
@@ -118,35 +113,39 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 //
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
-   hInst = hInstance; // Store instance handle in our global variable
+	hInst = hInstance; // Store instance handle in our global variable
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-	  CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+	HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
 
-   if (!hWnd)
-   {
-	  return FALSE;
-   }
+	if (!hWnd)
+	{
+		return FALSE;
+	}
 
-   RECT rect;
-   GetClientRect(hWnd, &rect);
-   int centerX = rect.right / 2;
-   int centerY = rect.bottom / 2;
-   buttonWidth = rect.right / 4;
-   buttonHeight = rect.bottom / 4;
+	updateCoords(hWnd, centerX, centerY, buttonHeight, buttonWidth);
 
-   hButton = CreateWindow(
-	   L"BUTTON", L"Start timer",
-	   WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
-	   centerX - (buttonWidth/2), centerY + (centerY / 2) - (buttonHeight / 2), buttonWidth, buttonHeight,
-	   hWnd, (HMENU)1, hInstance, nullptr);
+	hButton = CreateWindow(
+		L"BUTTON", L"Start",
+		WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+		centerX - (buttonWidth / 2), (centerY * 1.5) - (buttonHeight / 2), buttonWidth, buttonHeight,
+		hWnd, (HMENU)1, hInstance, nullptr);
 
-   SetTimer(hWnd, 2, 1000, NULL);
+	updateFontSize(fontSize);
+	buttonFont = CreateFont(
+		fontSize, // 12-point font
+		0, 0, 0, 400, FALSE, FALSE, FALSE,
+		DEFAULT_CHARSET, OUT_DEFAULT_PRECIS,
+		CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY,
+		DEFAULT_PITCH | FF_DONTCARE, L"Times New Roman");
 
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
+	SendMessage(hButton, WM_SETFONT, (WPARAM)buttonFont, TRUE);
 
-   return TRUE;
+	SetTimer(hWnd, 2, 1000, NULL);
+
+	ShowWindow(hWnd, nCmdShow);
+
+	return TRUE;
 }
 
 //
@@ -162,59 +161,72 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	switch (message)
-	{
+	{	
 	case WM_COMMAND:
+	{
+		int wmId = LOWORD(wParam);
+		// Parse the menu selections:
+		switch (wmId)
 		{
-			int wmId = LOWORD(wParam);
-			// Parse the menu selections:
-			switch (wmId)
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		case 1:
+			if (!timerState)
 			{
-			case IDM_ABOUT:
-				DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-				break;
-			case IDM_EXIT:
-				DestroyWindow(hWnd);
-					break;
-			case 1:
-				if (!timerState)
-				{
-					SetWindowText(hButton, L"Stop");
-					timerState = true;
-				}
-				else
-				{
-					SetWindowText(hButton, L"Start");
-					timerState = false;
-				}
-
-				break;
-			default:
-				return DefWindowProc(hWnd, message, wParam, lParam);
+				SetWindowText(hButton, L"Stop");
+				timerState = true;
 			}
+			else
+			{
+				SetWindowText(hButton, L"Start");
+				timerState = false;
+			}
+
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
 		}
-		break;
+	}
+	break;
 	case WM_PAINT:
-		{
-			PAINTSTRUCT ps;
-			HDC hdc = BeginPaint(hWnd, &ps);
-			// TODO: Add any drawing code that uses hdc here...
+	{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hWnd, &ps);
+		// TODO: Add any drawing code that uses hdc here...
 
-			draw(hdc, ps);
+		draw(hdc, hWnd);
 
-			EndPaint(hWnd, &ps);
-		}
-		break;
+		EndPaint(hWnd, &ps);
+	}
+	break;
 	case WM_SIZE:
-		RECT rect;
-		GetClientRect(hWnd, &rect);
+		updateCoords(hWnd, centerX, centerY, buttonHeight, buttonWidth);
+		MoveWindow(hButton, centerX - (buttonWidth / 2), (centerY * 1.5) - (buttonHeight / 2), buttonWidth, buttonHeight, TRUE);
 
-		MoveWindow(hButton, (rect.right / 2) - (buttonWidth / 2), (rect.bottom / 2) - (buttonHeight / 2), buttonWidth, buttonHeight, TRUE);
+		break;
+	case WM_EXITSIZEMOVE:
+		if (buttonFont)
+			DeleteObject(buttonFont);
 
-		buttonWidth = rect.right / 4;
-		buttonHeight = rect.bottom / 4;
+		updateFontSize(fontSize);
+		buttonFont = CreateFont(
+			fontSize, // 12-point font
+			0, 0, 0, 400, FALSE, FALSE, FALSE,
+			DEFAULT_CHARSET, OUT_DEFAULT_PRECIS,
+			CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY,
+			DEFAULT_PITCH | FF_DONTCARE, L"Times New Roman");
+
+		SendMessage(hButton, WM_SETFONT, (WPARAM)buttonFont, FALSE);
+		InvalidateRect(hWnd, nullptr, TRUE);
 
 		break;
 	case WM_DESTROY:
+		if (hbrBackground) DeleteObject(hbrBackground);
+		if (buttonFont) DeleteObject(buttonFont);
 		PostQuitMessage(0);
 		break;
 	case WM_TIMER:
@@ -224,11 +236,11 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		if (!timerState)
 			break;
 
-		if (countdown > 0)
-			countdown--;
-
+		if (countdown < 0)
+			break;
+		
+		countdown--;
 		InvalidateRect(hWnd, nullptr, TRUE);
-		UpdateWindow(hWnd);
 
 		break;
 	default:
@@ -237,18 +249,32 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	return 0;
 }
 
-void draw(HDC &hdc, PAINTSTRUCT &ps)
+void draw(HDC& hdc, HWND hWnd)
 {
 	SetBkMode(hdc, TRANSPARENT);
-	
-	for (auto message : messages)
-	{
-		TextOut(hdc, message.x, message.y, msg::str, wcslen(msg::str));
-	}
+	SelectObject(hdc, buttonFont);
 
 	wchar_t buffer[32];
-	swprintf_s(buffer, L"Time: %d", countdown);
-	TextOut(hdc, 10, 10, buffer, wcslen(buffer));
+	swprintf_s(buffer, L"Time: %02d:%02d", countdown / 60, countdown % 60);
+
+	RECT rect;
+	GetClientRect(hWnd, &rect);
+	DrawText(hdc, buffer, -1, &rect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+}
+
+void updateCoords(HWND hWnd, int& centerX, int& centerY, int& buttonHeight, int& buttonWidth)
+{
+	GetClientRect(hWnd, &rect);
+	centerX = rect.right / 2;
+	centerY = rect.bottom / 2;
+
+	buttonWidth = rect.right / 6;
+	buttonHeight = rect.bottom / 6;
+}
+
+void updateFontSize(int& fontSize)
+{
+	fontSize = buttonHeight * -0.5;
 }
 
 // Message handler for about box.
